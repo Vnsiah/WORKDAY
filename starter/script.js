d9c1be36aafee1ec6963148736e1de0beae82391
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-01-24 15:20:50 +0000

    currentdate with cssstyles

--- starter/script.js
+++ starter/script.js
@@ -17,4 +17,30 @@
 
 let timeblocks = [{hour:12,description:"I have a meeting"}, {hour:13,description:""},{hour:14,description:""},{hour:15,description:""},{hour:16,description:""},{hour:17,description:""}];
 
+let currentDate = moment().format("dddd, MMMM Do YYYY");
+// console.log(currentDate);
+$("#currentDay").text(currentDate);
+let currentTime = moment().format("H");
+let textarea = $(this).siblings('.description').val();
+let PlannerData = ""
+
+
+
+for (let i = 0; i < timeblocks.length; i++) {
+const timeblock = timeblocks[i];
+
+$(`<div class="row time-block future">
+<div class="col-2 hour">${timeblock.hour+"PM"}</div>
+<textarea class="col-9 description">${timeblock.description}</textarea>
+<button class="btn saveBtn col-1">
+    <i class="fa fa-save"></i>
+</button>
+</div>`).appendTo(".container")
+
+
+}
+
+
+
+
 
